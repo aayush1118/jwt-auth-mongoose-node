90801d7809918460922e6660321cea3cb5ec97c6
Aayush jain <aayushjain1118@gmail.com>
2021-06-12 20:55:23 +0530

    refresh token model

--- app/models/index.js
+++ app/models/index.js
@@ -5,9 +5,10 @@ const db = {};
 
 db.mongoose = mongoose;
 
-db.user = require("./user.model");
-db.role = require("./role.model");
+db.user = require('./user.model');
+db.role = require('./role.model');
+db.refreshToken = require('./refreshToken.model');
 
-db.ROLES = ["user", "admin", "moderator"];
+db.ROLES = ['user', 'admin', 'moderator'];
 
 module.exports = db;
