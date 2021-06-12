c56f72ec0b13ca887d544dada473277710e6e64c
Aayush jain <aayushjain1118@gmail.com>
2021-06-12 20:51:31 +0530

    refreshToken function

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,109 +1,167 @@
-const config = require("../config/auth.config");
-const db = require("../models");
-const User = db.user;
-const Role = db.role;
+const config = require('../config/auth.config');
+const db = require('../models');
+const { user: User, role: Role, refreshToken: RefreshToken } = db;
 
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
 exports.signup = (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
-  });
+	const user = new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: bcrypt.hashSync(req.body.password),
+	});
 
-  user.save((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
+	user.save((err, user) => {
+		if (err) {
+			res.status(500).send({ message: err });
+			return;
+		}
 
-    if (req.body.roles) {
-      Role.find(
-        {
-          name: { $in: req.body.roles }
-        },
-        (err, roles) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
+		if (req.body.roles) {
+			Role.find(
+				{
+					name: { $in: req.body.roles },
+				},
+				(err, roles) => {
+					if (err) {
+						res.status(500).send({
+							message: err,
+						});
+						return;
+					}
 
-          user.roles = roles.map(role => role._id);
-          user.save(err => {
-            if (err) {
-              res.status(500).send({ message: err });
-              return;
-            }
+					user.roles = roles.map((role) => role._id);
+					user.save((err) => {
+						if (err) {
+							res.status(500).send({
+								message: err,
+							});
+							return;
+						}
 
-            res.send({ message: "User was registered successfully!" });
-          });
-        }
-      );
-    } else {
-      Role.findOne({ name: "user" }, (err, role) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
+						res.send({
+							message: 'User was registered successfully!',
+						});
+					});
+				}
+			);
+		} else {
+			Role.findOne({ name: 'user' }, (err, role) => {
+				if (err) {
+					res.status(500).send({ message: err });
+					return;
+				}
 
-        user.roles = [role._id];
-        user.save(err => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
+				user.roles = [role._id];
+				user.save((err) => {
+					if (err) {
+						res.status(500).send({
+							message: err,
+						});
+						return;
+					}
 
-          res.send({ message: "User was registered successfully!" });
-        });
-      });
-    }
-  });
+					res.send({
+						message: 'User was registered successfully!',
+					});
+				});
+			});
+		}
+	});
 };
 
 exports.signin = (req, res) => {
-  User.findOne({
-    username: req.body.username
-  })
-    .populate("roles", "-__v")
-    .exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
+	User.findOne({
+		username: req.body.username,
+	})
+		.populate('roles', '-__v')
+		.exec(async (err, user) => {
+			if (err) {
+				res.status(500).send({ message: err });
+				return;
+			}
 
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
+			if (!user) {
+				return res.status(404).send({ message: 'User Not found.' });
+			}
 
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
+			var passwordIsValid = bcrypt.compareSync(
+				req.body.password,
+				user.password
+			);
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!"
-        });
-      }
+			if (!passwordIsValid) {
+				return res.status(401).send({
+					accessToken: null,
+					message: 'Invalid Password!',
+				});
+			}
 
-      var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
-      });
+			var token = jwt.sign({ id: user.id }, config.secret, {
+				expiresIn: config.jwtExpiration,
+			});
 
-      var authorities = [];
+			let refreshToken = await RefreshToken.createToken(user);
 
-      for (let i = 0; i < user.roles.length; i++) {
-        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-      }
-      res.status(200).send({
-        id: user._id,
-        username: user.username,
-        email: user.email,
-        roles: authorities,
-        accessToken: token
-      });
-    });
+			var authorities = [];
+
+			for (let i = 0; i < user.roles.length; i++) {
+				authorities.push('ROLE_' + user.roles[i].name.toUpperCase());
+			}
+			res.status(200).send({
+				id: user._id,
+				username: user.username,
+				email: user.email,
+				roles: authorities,
+				accessToken: token,
+				refreshToken: refreshToken,
+			});
+		});
+};
+
+exports.refreshToken = async (req, res) => {
+	const { refreshToken: requestToken } = req.body;
+
+	if (requestToken == null) {
+		return res.status(403).json({ message: 'Refresh Token is required!' });
+	}
+
+	try {
+		let refreshToken = await RefreshToken.findOne({
+			token: requestToken,
+		});
+
+		if (!refreshToken) {
+			res.status(403).json({
+				message: 'invalid refresh token!',
+			});
+			return;
+		}
+
+		if (RefreshToken.verifyExpiration(refreshToken)) {
+			RefreshToken.findByIdAndRemove(refreshToken._id, {
+				useFindAndModify: false,
+			}).exec();
+
+			res.status(403).json({
+				message:
+					'Refresh token was expired. Please make a new signIn request',
+			});
+			return;
+		}
+
+		let newAccessToken = jwt.sign(
+			{ id: refreshToken.user._id },
+			config.secret,
+			{ expiresIn: config.jwtExpiration }
+		);
+
+		return res.status(200).json({
+			accessToken: newAccessToken,
+			refreshToken: refreshToken.token,
+		});
+	} catch (err) {
+		return res.status(500).send({ message: err });
+	}
 };
